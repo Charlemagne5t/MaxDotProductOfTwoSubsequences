08f401de551c9be033fb4383d91c9f81aa6e37c2
alek <wait82985@gmail.com>
2023-10-08 14:43:01 +0600

    problem solved. Top down dp

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -32,4 +32,14 @@ public class SolutionTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void maxDotProductTest4() {
+        int[] nums1 = {13,-7,12,-15,-7,8,3,-7,-5,13,-15,-8,5,7,-1,3,-11,-12,2,-12};
+        int[] nums2 = {-1,13,-4,-2,-13,2,-4,6,-9,13,-8,-3,-9};
+        int expected = 972;
+        int actual = new Solution().maxDotProduct(nums1, nums2);
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
